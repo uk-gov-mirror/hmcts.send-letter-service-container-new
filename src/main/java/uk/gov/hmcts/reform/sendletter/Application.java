a0251c1656cf2c0b5014a18bdb7d257afc6c4d01
SomeshRA <somesh.acharya1@hmcts.net>
2021-04-15 11:06:05 +0100

    DTSBPS-487:For NEW container configure KEDA trigger

--- src/main/java/uk/gov/hmcts/reform/sendletter/Application.java
+++ src/main/java/uk/gov/hmcts/reform/sendletter/Application.java
@@ -1,15 +1,8 @@
 package uk.gov.hmcts.reform.sendletter;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.hystrix.EnableHystrix;
-
-@SpringBootApplication
-@EnableHystrix
-@SuppressWarnings("HideUtilityClassConstructor") // Spring needs a constructor, its not a utility class
 public class Application {
 
     public static void main(final String[] args) {
-        SpringApplication.run(Application.class, args);
+        System.out.println("Invoked");
     }
 }
